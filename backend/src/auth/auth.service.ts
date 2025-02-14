9373f0a7b3a92e5f6a14a172ac41843a3ef771e6
Erc4 <ericamezcua3@gmail.com>
2025-02-14 14:36:47 -0700

    Arreglo del login username y password

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -8,16 +8,16 @@ export class AuthService {
 constructor(
 private usersService: UsersService,
 private jwtService: JwtService,
-) {}
+) {}    
 
-async validateUser(email: string, password: string): Promise<any> {
-const user = await this.usersService.findByEmail(email);
-if (user && (await bcrypt.compare(password, user.password))) {
-    const { password, ...result } = user;
-    return result;
-}
-return null;
-}
+async validateUser(username: string, password: string): Promise<any> {
+    const user = await this.usersService.findByUsername(username);
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
 
 async login(user: any) {
     const payload = {

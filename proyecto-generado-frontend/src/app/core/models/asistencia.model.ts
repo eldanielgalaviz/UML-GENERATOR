ff18a42cf6e417e54fd4c4395e9462be950fa198
eldanielgalaviz <pvcdanielgalaviz@gmail.com>
2025-03-12 21:02:42 -0700

    Delete proyecto-generado-frontend directory

--- proyecto-generado-frontend/src/app/core/models/asistencia.model.ts
+++
@@ -1,6 +0,0 @@
-export interface Asistencia {
-  id: number;
-  fecha: string; // Store as string in 'YYYY-MM-DD' format
-  presente: boolean;
-  usuarioId: number; // Foreign key to Usuario
-}
--- proyecto-generado-frontend/src/app/core/models/horario.model.ts
+++
@@ -1,7 +0,0 @@
-export interface Horario {
-  id: number;
-  grupo: string;
-  clase: string;
-  horaInicio: string; // Store as string in 'HH:mm' format
-  horaFin: string;   // Store as string in 'HH:mm' format
-}
